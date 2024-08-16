af2c2568c3057cc09a9f5176308520d9e29a2e6c
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-08-16 07:53:02 -0700

    refactor: send erc20 via precompile

--- scripts/send_erc20_via_precompile.ts
+++ scripts/send_erc20_via_precompile.ts
@@ -9,11 +9,14 @@ const mnemonic = "guard cream sadness conduct invite crumble clock pudding hole 
 const owner = HDNodeWallet.fromPhrase(mnemonic, "", "m/44'/118'/0'/0/0").connect(jsonRpcProvider)
 Wallet.fromPhrase(mnemonic, jsonRpcProvider)
 
+const FunTokenGatewayAddress = "0x0000000000000000000000000000000000000800"
+const ERC20Address = "0x7D4B7B8CA7E1a24928Bb96D59249c7a5bd1DfBe6"
+const to = "nibi1gc6vpl9j0ty8tkt53787zps9ezc70kj88hluw4"
 
 async function main() {
-  const contract = IFunToken__factory.connect("0x0000000000000000000000000000000000000800", owner)
+  const contract = IFunToken__factory.connect(FunTokenGatewayAddress, owner)
 
-  const txResponse = await contract.bankSend("0x7D4B7B8CA7E1a24928Bb96D59249c7a5bd1DfBe6", 1, "nibi1gc6vpl9j0ty8tkt53787zps9ezc70kj88hluw4")
+  const txResponse = await contract.bankSend(ERC20Address, 1, to)
   console.log(txResponse)
   console.log(await jsonRpcProvider.waitForTransaction(txResponse.hash))
 }
