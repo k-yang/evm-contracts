e8e18a4a45bf7b322916936c93d9b6684f543921
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-08-16 07:45:40 -0700

    feat: add localnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    nibiru: {
+      url: "http://localhost:8545",
+      accounts: {
+        mnemonic: "guard cream sadness conduct invite crumble clock pudding hole grit liar hotel maid produce squeeze return argue turtle know drive eight casino maze host",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    }
+  }
 };
 
 export default config;
