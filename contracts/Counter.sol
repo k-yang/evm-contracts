48b4a2c0886a7d46354e108310f273aed6ab6983
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-11-11 10:07:51 -0800

    update Counter contract

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -4,16 +4,23 @@ pragma solidity ^0.8.24;
 contract Counter {
     uint256 public counter = 0;
 
+    event Increment(address indexed from, uint256 by);
+    event Decrement(address indexed from, uint256 by);
+    event Reset(address indexed from);
+
     function increment(uint256 by) public {
         counter += by;
+        emit Increment(msg.sender, by);
     }
 
     function decrement(uint256 by) public {
         counter -= by;
+        emit Decrement(msg.sender, by);
     }
 
     function reset() public {
         counter = 0;
+        emit Reset(msg.sender);
     }
 
     function get() public view returns (uint256) {
