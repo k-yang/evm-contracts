daacd51604352ff103e49c866229668002866123
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-11-11 10:07:25 -0800

    add wasm estimate gas scripts

---
+++ scripts/wasm/estimate-gas-infinite-loop.ts
@@ -0,0 +1,27 @@
+import { HDNodeWallet, JsonRpcProvider, toUtf8Bytes } from "ethers";
+import { IWasm__factory } from "../../typechain-types";
+
+// connects to local node
+// const jsonRpcProvider = new JsonRpcProvider("https://evm-rpc.devnet-1.nibiru.fi:443");
+const jsonRpcProvider = new JsonRpcProvider("http://localhost:8545");
+
+// mnemonic for the HD wallet
+const mnemonic = "guard cream sadness conduct invite crumble clock pudding hole grit liar hotel maid produce squeeze return argue turtle know drive eight casino maze host"
+const owner = HDNodeWallet.fromPhrase(mnemonic, "", "m/44'/118'/0'/0/0").connect(jsonRpcProvider)
+
+// get command line arguments
+const COMMAND_LINE_ARGS = process.argv.slice(2)
+const CONTRACT_ADDR = COMMAND_LINE_ARGS[0]
+
+async function main() {
+  const wasmPrecompile = IWasm__factory.connect("0x0000000000000000000000000000000000000802", owner);
+
+  console.log("run:", await wasmPrecompile.execute.estimateGas(
+    CONTRACT_ADDR,
+    toUtf8Bytes(JSON.stringify({
+      "run": {}
+    })),
+    []));
+}
+
+main()
