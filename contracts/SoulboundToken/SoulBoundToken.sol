ec7ed446c2ad754a88402fee3d94e461b3057e57
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2025-01-20 12:31:47 -0800

    add soulbound erc721

---
+++ contracts/SoulboundToken/SoulBoundToken.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NibiVDoe is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("https://nibiru.fi/docs/community/legal/2024-nibi-v-doe.html", "NIBI-v-DOE")
+        Ownable(msg.sender)
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721) virtual returns (address) {
+        address from = _ownerOf(tokenId);
+        if (from != address(0)) {
+            revert("Err: token transfer is BLOCKED");
+        }
+        return super._update(to, tokenId, auth);
+    }
+
+    function setTokenURI(uint256 tokenId, string memory newTokenURI) public onlyOwner returns (uint256) {
+        _setTokenURI(tokenId, newTokenURI);
+        return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
