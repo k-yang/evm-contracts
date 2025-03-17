b9477bc81ba623da0bb7213f2920af58636d415e
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2025-03-17 13:36:39 -0400

    fix: update erc20 sendTo* scripts

--- scripts/erc20/sendToBank.ts
+++ scripts/erc20/sendToBank.ts
@@ -11,18 +11,20 @@ Wallet.fromPhrase(mnemonic, jsonRpcProvider)
 
 const FunTokenGatewayAddress = "0x0000000000000000000000000000000000000800"
 const ERC20Address = "0x76e03400dC49dD3Dbede29f5e11b0e7bc215F202"
-const to = "nibi1ltez0kkshywzm675rkh8rj2eaf8et78cqjqrhc"
+const bankDenom = "erc20/" + ERC20Address
+const recipient = "nibi1ltez0kkshywzm675rkh8rj2eaf8et78cqjqrhc"
 
 async function main() {
   console.log("owner address:", owner.address)
   const funtokenPrecompile = IFunToken__factory.connect(FunTokenGatewayAddress, owner)
 
-  const txResponse = await funtokenPrecompile.sendToBank(ERC20Address, 1, to)
+  const txResponse = await funtokenPrecompile.sendToBank(ERC20Address, 1, recipient)
   console.log("txResponse: ", txResponse)
+
   const receipt = await jsonRpcProvider.waitForTransaction(txResponse.hash)
   console.log("receipt: ", receipt)
-  console.log("balanceOf(recipient): ", await funtokenPrecompile.bankBalance("0xFaF227daD0b91C2dEBD41daE71C959EA4f95f8F8", "erc20/" + ERC20Address))
 
+  console.log("balanceOf(recipient): ", await funtokenPrecompile.bankBalance("0xFaF227daD0b91C2dEBD41daE71C959EA4f95f8F8", bankDenom))
   const erc20 = MyToken__factory.connect(ERC20Address, owner)
   console.log("balanceOf(owner): ", await erc20.balanceOf(owner.address))
 }
