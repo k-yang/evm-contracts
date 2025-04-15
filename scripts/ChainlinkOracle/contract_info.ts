ffaf03b0e89d4b8085480fcb7a955e3e670a595f
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2025-04-15 11:34:49 -0700

    rename chainlink oracle contract

--- scripts/ChainlinkOracle/contract_info.ts
+++ scripts/ChainlinkOracle/contract_info.ts
@@ -1,21 +1,18 @@
-import { HDNodeWallet, JsonRpcProvider } from "ethers";
-import { NibiruOracleChainLinkLike__factory } from "../../typechain-types";
+import { JsonRpcProvider } from "ethers";
+import { ChainLinkAggregatorV3Interface__factory } from "../../typechain-types";
 
 // connects to local node
 // const jsonRpcProvider = new JsonRpcProvider("http://localhost:8545");
 const jsonRpcProvider = new JsonRpcProvider("https://evm-rpc.nibiru.fi:443");
 
-// mnemonic for the HD wallet
-const mnemonic = "guard cream sadness conduct invite crumble clock pudding hole grit liar hotel maid produce squeeze return argue turtle know drive eight casino maze host"
-const owner = HDNodeWallet.fromPhrase(mnemonic, "", "m/44'/118'/0'/0/0").connect(jsonRpcProvider)
-console.log("owner address:", owner.address)
-
 const nibiUsd = "0x1ee4787898Fa3082e3af8a04e19450b17E97E70a";
 const ethUsd = "0x0a713B47d49944908a983F5a42bF743b590137BE";
 const btcUsd = "0x445Ca1db886ED3f08b8957e0581c25AfC6f48081";
+const usdcUsd = "0x22A1eBBe1282d9E4EC64FedF71826E1faD056Eb1";
+const usdtUsd = "0x86C6814Aa44fA22f7B9e0FCEC6F9de6012F322f8";
 
 async function main() {
-  const contract = NibiruOracleChainLinkLike__factory.connect(btcUsd, owner)
+  const contract = ChainLinkAggregatorV3Interface__factory.connect(usdtUsd, jsonRpcProvider)
   console.log("contract address: ", await contract.getAddress())
 
   console.log("decimals: ", await contract.decimals())
--- scripts/ChainlinkOracle/deploy.ts
+++ scripts/ChainlinkOracle/deploy.ts
@@ -2,9 +2,9 @@ import { HDNodeWallet, JsonRpcProvider } from "ethers";
 import { NibiruOracleChainLinkLike__factory } from "../../typechain-types";
 
 // connects to local node
-// const jsonRpcProvider = new JsonRpcProvider("https://evm-rpc.testnet-2.nibiru.fi:443");
+const jsonRpcProvider = new JsonRpcProvider("https://evm-rpc.testnet-2.nibiru.fi:443");
 // const jsonRpcProvider = new JsonRpcProvider("http://localhost:8545");
-const jsonRpcProvider = new JsonRpcProvider("https://evm-rpc.nibiru.fi:443");
+// const jsonRpcProvider = new JsonRpcProvider("https://evm-rpc.nibiru.fi:443");
 
 // mnemonic for the HD wallet
 const mnemonic = "guard cream sadness conduct invite crumble clock pudding hole grit liar hotel maid produce squeeze return argue turtle know drive eight casino maze host"
@@ -13,7 +13,7 @@ console.log("owner address:", owner.address)
 
 async function main() {
   const factory = new NibiruOracleChainLinkLike__factory(owner);
-  const contract = await factory.deploy("ubtc:uusd", 8, {
+  const contract = await factory.deploy("uusdt:uusd", 8, {
     gasPrice: "1",
   });
   const addr = await contract.getAddress()
