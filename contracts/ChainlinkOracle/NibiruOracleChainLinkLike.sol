eda563af1c953c73a873cf14fc0dda40cd1cfbe6
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2025-04-15 11:39:41 -0700

    Update NibiruOracleChainLinkLike.sol

--- contracts/ChainlinkOracle/NibiruOracleChainLinkLike.sol
+++ contracts/ChainlinkOracle/NibiruOracleChainLinkLike.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
-import "./Precompiles/IOracle.sol";
+import "../Precompiles/IOracle.sol";
 
 /// @title NibiruOracleChainLinkLike
 /// @notice This contract serves as a ChainLink-like data feed that sources its
